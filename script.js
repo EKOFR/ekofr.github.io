62ac8bf02d17e733974822b1d0af7d470f02e72b
EKOFR <contact.ekofr@gmail.com>
2026-08-28 11:49:51 +0200

    Change class toggle from 'ouvrir' to 'active'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const navigation = document.querySelector('.nav-links');
   if (bouton && navigation) {
     bouton.addEventListener('click', () => {
-      navigation.classList.toggle('ouvrir');
+      
       navigation.classList.toggle('active');
     });
   }
